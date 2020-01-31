24c49101f11017a4208da48a4b3193dd6c77baf2
gothaw <rad.soltan@gmail.com>
2020-01-31 21:00:55 +0000

    Added insert query for song table.

--- scripts/insert_into_db.sql
+++ scripts/insert_into_db.sql
@@ -100,4 +100,47 @@ INSERT INTO `metal_rock_db`.`song` (
     `so_link_lyrics`,
     `album_al_id`
 ) VALUES
-	('Mirror Mirror','9','306',NULL,'https://www.lyrics.com/lyric/2555730/Blind+Guardian/Mirror+Mirror','4');
+	('Mirror Mirror','9','306','https://youtu.be/CNGCvI9dpZY','https://www.lyrics.com/lyric/2555730/Blind+Guardian/Mirror+Mirror','4'),
+    ('Black Sabbath','1','380','https://youtu.be/0lVdMbUx1_k',NULL,'1'),
+    ('Number of the Beast','5','265','https://youtu.be/WxnN05vOuSM','https://www.lyrics.com/lyric/673078/Iron+Maiden/Number+of+the+Beast','3'),
+    ('Run to the Hills','6','230','https://youtu.be/86URGgqONvA','https://www.lyrics.com/lyric/518185/Iron+Maiden/Run+to+the+Hills','3'),
+    ('Majesty','1','448','https://youtu.be/ozOXyQPLISc',NULL,'1'),
+    ('Nightfall','4','334','https://youtu.be/M0TcB5lxfuY','https://www.lyrics.com/lyric/2555725/Blind+Guardian/Nightfall','4'),
+    ('N.I.B.','4','368','https://youtu.be/MiY2JsGXrtM','https://www.lyrics.com/lyric/20506/Black+Sabbath/N.I.B.','4'),
+    ('Hallowed Be Thy Name','8','428','https://youtu.be/HAQQUDbuudY','https://www.lyrics.com/lyric/817713/Iron+Maiden/Hallowed+Be+Thy+Name','3'),
+    ('Afraid to Shoot Strangers','3','412','https://youtu.be/0c9iYZdsZMM','https://www.lyrics.com/lyric/817720/Iron+Maiden/Afraid+to+Shoot+Strangers','5'),
+    ('Fear of the Dark','12','436','https://youtu.be/bePCRKGUwAY','https://www.lyrics.com/lyric/817724/Iron+Maiden/Fear+of+the+Dark','5'),
+    ('Heaven and Hell','4','419','https://youtu.be/uWAhd4KkVUU','https://www.lyrics.com/lyric/872026/Black+Sabbath/Heaven+and+Hell','6'),
+    ('Die Young','6','285',NULL,'https://www.lyrics.com/lyric/872028/Black+Sabbath/Die+Young','6'),
+    ('Into the Storm','2','264','https://youtu.be/Yx1PCWkOb3Y','https://www.lyrics.com/lyric/2555723/Blind+Guardian/Into+the+Storm','4'),
+    ('Run for the Night','5','213','https://youtu.be/MfKxRVZEoZA',NULL,'1'),
+    ('And Then There Was Silence','10','846','https://youtu.be/HcRIGCmCoZA','https://www.lyrics.com/lyric/5413849/Blind+Guardian/And+Then+There+Was+Silence','7'),
+    ('Time Stands Still (At the Iron Hill)','13','293','https://youtu.be/6Yz4_1mZarA','https://www.lyrics.com/lyric/2555734/Blind+Guardian/Time+Stands+Still+%28At+the+Iron+Hill%29','4'),
+    ('Battlefield','2','337','https://youtu.be/64PQZwjrF2E','https://www.lyrics.com/track/5413841/Blind+Guardian/Battlefield','7'),
+    ('Dance of Death','5','516','https://youtu.be/3659fTXvFts',NULL,'9'),
+    ('Paschendale','8','507','https://youtu.be/oEFmrm_GdUw',NULL,'9'),
+    ('Face in the Sand','9','391',NULL,NULL,'9'),
+    ('For Whom the Bell Tolls','3','310','https://youtu.be/eeqGuaAl6Ic',NULL,'10'),
+    ('Fade to Black','4','415','https://youtu.be/HdWw9SksiwQ',NULL,'10'),
+    ('Creeping Death','7','396','https://youtu.be/xAOMEqMXiXI',NULL,'10'),
+    ('The Call of Ktulu','8','533','https://youtu.be/TfGMinbSVI8',NULL,'10'),
+    ('Until It Sleeps','4','268',NULL,NULL,'12'),
+    ('Bleeding Me','7','498',NULL,NULL,'12'),
+    ('Rainbow in the Dark','8','255',NULL,'https://www.lyrics.com/lyric/877609/Dio/Rainbow+in+the+Dark','11');
+    
+    
+
+-- -----------------------------------------------------
+-- Insert into `musician`
+-- -----------------------------------------------------
+
+INSERT INTO `metal_rock_db`.`song` (
+	`mu_id`,
+    `mu_first_name`,
+    `mu_last_name`,
+    `mu_city_origin`,
+    `mu_country_origin`,
+    `mu_date_of_birth`,
+    `mu_date_of_death`
+) VALUES
+	('Mirror Mirror','9','306','https://youtu.be/CNGCvI9dpZY','https://www.lyrics.com/lyric/2555730/Blind+Guardian/Mirror+Mirror','4');
